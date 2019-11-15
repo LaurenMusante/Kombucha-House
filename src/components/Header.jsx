a2da9f982e9cbf95cf001dc75af9dcff8c4a779c
Lauren <Lauren.Musante@gmail.com>
2019-11-15 11:35:49 -0800

    Style Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div>
-            <h1>What would you like to do?</h1>
+            <h2>Welcome to Lauren's Kombucha House</h2>
+            <div className='card'>
+                <div className='card-header'>
+                    <h1>What would you like to do?</h1>
+                </div>    
             <h1><Link to='/keglist'>View Current Kegs</Link></h1>
             <h1><Link to='/newkegform'>Add Keg</Link></h1>
+            </div>
         </div>
     );
 }
