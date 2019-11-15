7c3e16cc4b8c48d2ce5209ed86b73a41e36c4ed8
Lauren <Lauren.Musante@gmail.com>
2019-11-15 08:57:20 -0800

    Add boilerplate code, install Webpack and plugins

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+//this is the entry file
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react-app-root')
+);
